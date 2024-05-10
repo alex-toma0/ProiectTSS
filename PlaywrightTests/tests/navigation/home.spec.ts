1ad73c5dd1dca82e5160c2c1d0ea4e5ba6ce7c84
alex-toma <alexandrutoma.ro@gmail.com>
2024-05-10 09:09:42 +0300

    Updated Navigation Test

--- PlaywrightTests/tests/navigation/home.spec.ts
+++ PlaywrightTests/tests/navigation/home.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from "@playwright/test";
 test("Home Navigation", async ({ page }) => {
+  const [email, password] = ["a2@a2.com", "a2"];
+
+  // Logs in
+  await page.goto("http://localhost:5173/login");
+  await page.getByLabel("Email address").fill(email);
+  await page.getByLabel("Password").fill(password);
+  // Click the login button
+  await page.getByRole("button", { name: "Login" }).click();
   // Checks if navigating to the Home page works as expected
-  await page.goto("http://localhost:5173/");
-  await page
-    .getByRole("link", {
-      name: "Streaming App",
-    })
-    .click();
-  await page.waitForURL("http://localhost:5173/");
+  await page.getByRole("link", { name: "Streaming App" }).click();
   await expect(
     page.getByRole("button", {
       name: "Genre",

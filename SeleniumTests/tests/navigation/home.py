0a74e0aa3d5acbc8b4b430c5151bfbed940e7231
razvanbilici <81765319+razvanbilici@users.noreply.github.com>
2024-05-11 10:27:50 +0300

    Update home.py

--- SeleniumTests/tests/navigation/home.py
+++ SeleniumTests/tests/navigation/home.py
@@ -1,39 +1,14 @@
 import time
 from time import sleep
 from typing import Callable
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-# ----------------------------
-email: str = "a2@a2.com"
-password: str = "a2"
-
-# email form id = "formBasicEmail"
-# password form id = "formBasicPassword"
-
-login_url: str = "http://localhost:5173/login"
-chrome_driver = webdriver.Chrome()
-chromium_driver = webdriver.Edge()
+from SeleniumTests.config import chromium_driver, chrome_driver
+from SeleniumTests.tests.auth.login import login
 
 # -------------
 def run_test(test_function: Callable):
     test_function()
 # -------------
-
-
-def login(browser) -> None:
-
-    browser.get(login_url)
-    email_form = browser.find_element(By.ID, "formBasicEmail")
-    pass_form = browser.find_element(By.ID, "formBasicPassword")
-    login_button = browser.find_element(By.XPATH, "//button[contains(text(), 'Login')]")
-
-    email_form.send_keys(email)
-    pass_form.send_keys(password)
-
-    login_button.click()
-
-    sleep(2)
 
 def home_tests(*browsers) -> None:
     for browser in browsers:

f65185d284bcdae473052609c7db931a99068439
razvanbilici <81765319+razvanbilici@users.noreply.github.com>
2024-05-11 11:08:35 +0300

    Updated login.py

--- SeleniumTests/tests/auth/login.py
+++ SeleniumTests/tests/auth/login.py
@@ -1,22 +1,17 @@
 from SeleniumTests.config import login_url, email_id, pass_id, email, password
 from selenium.webdriver.common.by import By
+from time import sleep
 
-def login(browser) -> bool:
+def login(browser) -> None:
 
-    try:
-        browser.get(login_url)
-        email_form = browser.find_element(By.ID, email_id)
-        pass_form = browser.find_element(By.ID, pass_id)
-        login_button = browser.find_element(By.XPATH, "//button[contains(text(), 'Login')]")
+    browser.get(login_url)
+    email_form = browser.find_element(By.ID, email_id)
+    pass_form = browser.find_element(By.ID, pass_id)
+    login_button = browser.find_element(By.XPATH, "//button[contains(text(), 'Login')]")
 
-        email_form.send_keys(email)
-        pass_form.send_keys(password)
+    email_form.send_keys(email)
+    pass_form.send_keys(password)
 
-        login_button.click()
-        genre_button = browser.find_element(By.XPATH, "//*[contains(text(), 'Genre')]")
+    login_button.click()
 
-        if (genre_button.is_displayed()):
-            return True
-        return False
-    except Exception as e:
-        print(f"Encountered error in {browser.name}: {e}")
+    sleep(2)

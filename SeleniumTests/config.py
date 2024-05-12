c29387ec3f9b458cf96c5ad96b34a75815d0a48b
razvanbilici <81765319+razvanbilici@users.noreply.github.com>
2024-05-12 23:13:36 +0300

    Updated config file

--- SeleniumTests/config.py
+++ SeleniumTests/config.py
@@ -6,10 +6,30 @@ password: str = "a2"
 
 email_id: str = "formBasicEmail"
 pass_id: str = "formBasicPassword"
+name_id: str = "formBasicName"
+confirm_pass_id: str = "formConfirmPassword"
+
+register_email: str = "testMail@test.com"
+register_name: str = "testName"
+register_pass: str = "testPass"
 
 login_url: str = "http://localhost:5173/login"
+register_url: str = "http://localhost:5173/register"
 
-# lambdas so the drivers don't get initialized every time they're imported 
+# lambdas so the drivers don't get initialized every time they're imported
 chrome_driver = lambda: webdriver.Chrome()
 edge_driver = lambda: webdriver.Edge()
+
+title_form_id: str = "formSongTitle"
+img_form_id: str = "formImagePath"
+genre_form_id: str = "formGenre"
+file_form_id: str = "formFile"
+
+song_title: str = "Sample"
+song_image: str = "https://raw.githubusercontent.com/alex-toma0/images/main/pop-cover.jpg"
+song_genre: str = "Pop"
+
+# Requires absolute path
+file_sample: str = "E:\Pycharm\Projects\pythonProject3\SeleniumTests\sample.ogg"
+
 

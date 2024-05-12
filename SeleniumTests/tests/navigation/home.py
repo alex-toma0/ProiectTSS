72d595e443383188457509c3f16c84755d30fc3c
razvanbilici <81765319+razvanbilici@users.noreply.github.com>
2024-05-12 17:40:43 +0300

    Moved testcases to standalone file

--- SeleniumTests/tests/navigation/home.py
+++ SeleniumTests/tests/navigation/home.py
@@ -1,20 +1,18 @@
 import time
 from time import sleep
-from typing import Callable
 from selenium.webdriver.common.by import By
 from SeleniumTests.tests.auth.login import login
-from SeleniumTests.config import *
-
-# -------------
-def run_test(test_function: Callable):
-    test_function()
-# -------------
 
 
-def home_tests(*browsers) -> None:
+def back_to_home_test(*browsers) -> None:
+
+    print("Button navigation test started")
+
     for browser in browsers:
         try:
             # Check Navigation - login - profile page - back to home page
+            browser = browser()
+            sleep(2)
             login(browser)
             profile = browser.find_element(By.XPATH, "//a[contains(text(), 'Profile')]")
             profile.click()
@@ -27,8 +25,7 @@ def home_tests(*browsers) -> None:
                 print(f"{browser.name}: {genre_button.text} button is visible")
             sleep(2)
         except Exception as e:
-            print(f"Encountered error in {browser.name}: {e}")
+            print(f"Encountered error in {browser.name}: {type(e).__name__}")
         finally:
+            print(browser.name + " Navigation test finished\n-------")
             browser.quit()
-# ---------------------------
-run_test(lambda: home_tests(chromium_driver, chrome_driver))

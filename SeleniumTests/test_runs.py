5a9c0b087853af15cdf56852a351fd9e67f8bb74
razvanbilici <81765319+razvanbilici@users.noreply.github.com>
2024-05-12 23:03:19 +0300

    Added all test runs

--- SeleniumTests/test_runs.py
+++ SeleniumTests/test_runs.py
@@ -1,6 +1,9 @@
 from typing import Callable
 from SeleniumTests.tests.navigation.home import back_to_home_test
 from SeleniumTests.tests.navigation.profile import profile_test
+from SeleniumTests.tests.auth.register import register_test
+from SeleniumTests.tests.functional.playback import playback
+from SeleniumTests.tests.functional.upload import upload
 from config import chrome_driver, edge_driver
 
 
@@ -9,3 +12,6 @@ def run_test(test_function: Callable):
 
 run_test(lambda: back_to_home_test(chrome_driver))
 run_test(lambda: profile_test(chrome_driver, edge_driver))
+run_test(lambda: register_test(chrome_driver))
+run_test(lambda: playback(chrome_driver))
+run_test(lambda: upload(chrome_driver))
